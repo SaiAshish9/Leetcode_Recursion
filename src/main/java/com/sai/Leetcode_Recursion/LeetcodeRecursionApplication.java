120e16c8a4e6a06fbef9c4874497312eaa4f1934
SaiAshish <saiashish3760@gmail.com>
2023-05-07 19:36:36 +0530

    Add files via upload

---
+++ src/main/java/com/sai/Leetcode_Recursion/LeetcodeRecursionApplication.java
@@ -0,0 +1,13 @@
+package com.sai.Leetcode_Recursion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeRecursionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeRecursionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/Leetcode_Recursion/LeetcodeRecursionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.Leetcode_Recursion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeRecursionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
